5d191e83b1f793f13ed6c3f6f048c2dbfe49b117
Rishi gupta <rg00663@gmail.com>
2026-02-02 12:26:48 +0530

    bug and ui fix

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const isLoggedIn = request.cookies.get("isLoggedIn");
 
   // Protect dashboard
